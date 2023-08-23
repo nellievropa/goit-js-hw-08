837fe45562c8a7a640d1ce3bd60617f5850aec5b
nellievropa <nellievropa23@gmail.com>
2023-08-23 13:04:59 +0300

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,18 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// import {sum as fnSum, add} from './02-video'
+// import * as data from './02-video'
+// const sum = 10;
+// console.log(galleryItems);
+// console.log(sum)
+// console.log(fnSum(4, 5));
+// console.log(add(2, 2));
+// console.log(data.add(6, 9));
+
+import value from './02-video';
+
+console.log(value)
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+// const value = 100;
+// function add(a, b) {
+//     return a + b;
+// }
 
+//  function sum(a, b) {
+//     return a * b + value;
+// }
+
+// export {add, sum};
+
+const a = 10;
+const b = 15;
+export default a;
+// export default b;
