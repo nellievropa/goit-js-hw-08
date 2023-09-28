220e894b86098d402501ed739b08a488853ec2d3
nellievropa <124687833+nellievropa@users.noreply.github.com>
2023-09-28 18:58:15 +0000

    Deploying to gh-pages from @ nellievropa/goit-js-hw-08@cfe99fd4515a96d6e555271e24874d3dd492bff0 🚀

---
+++ index.41a88fb7.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".session"),o=document.querySelector(".local");e.addEventListener("click",(function(){sessionStorage.setItem("session","hello session")})),o.addEventListener("click",(function(){localStorage.setItem("local","hello Local")}))}();
+//# sourceMappingURL=index.41a88fb7.js.map
---
+++ index.e507de8f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".session"),o=document.querySelector(".local");e.addEventListener("click",(function(){sessionStorage.setItem("session","hello session")})),o.addEventListener("click",(function(){localStorage.setItem("local","hello Local")}));
+//# sourceMappingURL=index.e507de8f.js.map
