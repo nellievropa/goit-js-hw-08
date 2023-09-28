7e4eb3a4a5d492ca3a13a5add438b2ff9dbc223a
nellievropa <nellievropa23@gmail.com>
2023-09-28 22:06:57 +0300

    +

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,14 @@ const session = document.querySelector('.session');
 const local = document.querySelector('.local');
 
 session.addEventListener('click', onSession);
-
+const arr =[1,2,3,4,5,]
 function onSession() {
-    sessionStorage.setItem('session', 'hello session');
-
+    // sessionStorage.setItem('session', 'hello session');
+    sessionStorage.setItem('session', arr);
 }
 
 local.addEventListener('click', onLocal);
 
 function onLocal() {
-    localStorage.setItem('local', 'hello Local')
+    localStorage.setItem('local', JSON.stringify(arr))
 }
