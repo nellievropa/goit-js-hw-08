c304b8551e996892c83cd501941c061398009125
nellievropa <nellievropa23@gmail.com>
2023-09-29 14:54:43 +0300

    localStorage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,38 @@
 
+
+const email = document.querySelector('input[type="email"]');
+email.addEventListener('input', onEmail);
+
+const message = document.querySelector('textarea[name="message"]');
+message.addEventListener('input', onMessage);
+function onMessage() {
+    localStorage.setItem('local', JSON.stringify())
+
+}
+
+const button = document.querySelector('button[type="submit"]');
+button.addEventListener('click', onClick);
+
+function onClick() {
+// localStorage.removeItem('local');
+// щоб видалити все
+localStorage.clear();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // створити функцію, яка приймає масив з данних про час витрачений на оплату покупки на касі і 2 параметр- кількість кас
 // підрахувати треба скільки часу буде працювати кожна каса
 // 1 каса 17+2+15 =29
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,35 @@
 const session = document.querySelector('.session');
 const local = document.querySelector('.local');
+const remove = document.querySelector('.remove');
 
 session.addEventListener('click', onSession);
 const arr =[1,2,3,4,5,]
 function onSession() {
     // sessionStorage.setItem('session', 'hello session');
-    sessionStorage.setItem('session', arr);
+    const data = JSON.parse(localStorage.getItem('local')) || [];
+    console.log(data);
+    // якщо це- const arr =[1,2,3,4,5,]-масив то й тут додаємо пустий масив 
+    // const resp = JSON.parse(data)
+    // console.log(resp);
+    // sessionStorage.setItem('session', arr);
+
+    data.forEach(element => {
+        console.log(element);
+    });
 }
 
 local.addEventListener('click', onLocal);
 
 function onLocal() {
-    localStorage.setItem('local', JSON.stringify(arr))
+    // приводимо до рядка
+    localStorage.setItem('local', JSON.stringify(arr));
+    localStorage.setItem('local1', JSON.stringify(arr));
+    localStorage.setItem('local2', JSON.stringify(arr));
+}
+
+remove.addEventListener('click', onRemove)
+
+function onRemove() {
+// localStorage.removeItem('local');
+localStorage.clear();
 }
