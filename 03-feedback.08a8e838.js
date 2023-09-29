b1c23e2628e4080689fde6bdd2ea33cc3034001d
nellievropa <124687833+nellievropa@users.noreply.github.com>
2023-09-29 11:55:09 +0000

    Deploying to gh-pages from @ nellievropa/goit-js-hw-08@c304b8551e996892c83cd501941c061398009125 🚀

--- 03-feedback.08a8e838.js
+++
@@ -1,2 +0,0 @@
-console.log(function(o,n){const c={};for(let o=1;o<=n;o+=1)c[o]=0;return console.log(c),o.forEach((o=>{const n=Object.values(c),e=Math.min(...n),l=n.indexOf(e)+1;console.log(l),c[l]+=o})),c}([12,3,5,6,2,15,7],2));
-//# sourceMappingURL=03-feedback.08a8e838.js.map
--- index.a1ec3f34.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".session"),t=document.querySelector(".local");e.addEventListener("click",(function(){sessionStorage.setItem("session",n)}));const n=[1,2,3,4,5];t.addEventListener("click",(function(){localStorage.setItem("local",JSON.stringify(n))}));
-//# sourceMappingURL=index.a1ec3f34.js.map
