e960212c315ef52cc4de1e3b2763f9f4a2574e3d
nellievropa <nellievropa23@gmail.com>
2023-10-02 16:40:31 +0300

    local storage

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,48 @@
-const session = document.querySelector('.session');
-const local = document.querySelector('.local');
-const remove = document.querySelector('.remove');
+// const session = document.querySelector('.session');
+// const local = document.querySelector('.local');
+// const remove = document.querySelector('.remove');
 
-session.addEventListener('click', onSession);
-const arr =[1,2,3,4,5,]
-function onSession() {
-    // sessionStorage.setItem('session', 'hello session');
-    const data = JSON.parse(localStorage.getItem('local')) || [];
-    console.log(data);
-    // якщо це- const arr =[1,2,3,4,5,]-масив то й тут додаємо пустий масив 
-    // const resp = JSON.parse(data)
-    // console.log(resp);
-    // sessionStorage.setItem('session', arr);
+// session.addEventListener('click', onSession);
+// const arr =[1,2,3,4,5,]
 
-    data.forEach(element => {
-        console.log(element);
-    });
+
+// function onSession() {
+//     // sessionStorage.setItem('session', 'hello session');
+//     const data = JSON.parse(localStorage.getItem('local')) || [];
+// console.log(data);
+//     // якщо це- const arr =[1,2,3,4,5,]-масив то й тут додаємо пустий масив 
+//     // список позначається ...
+//     // const resp = JSON.parse(data)
+//     // console.log(resp);
+//     // sessionStorage.setItem('session', arr);
+
+//     data.forEach(element => {
+//         console.log(element);
+//     });
+// }
+
+// local.addEventListener('click', onLocal);
+
+// function onLocal() {
+//     // приводимо до рядка
+//     localStorage.setItem('local', JSON.stringify(arr));
+//     localStorage.setItem('local1', JSON.stringify(arr));
+//     localStorage.setItem('local2', JSON.stringify(arr));
+// }
+
+// remove.addEventListener('click', onRemove)
+
+// function onRemove() {
+//     // видаляем за назвою ключа
+// // localStorage.removeItem('local1');
+// // повністю чистимо локалсторадж
+// localStorage.clear();
+// }
+
+try {
+    console.log(a);
+}catch(e){
+    console.log(e);
 }
 
-local.addEventListener('click', onLocal);
-
-function onLocal() {
-    // приводимо до рядка
-    localStorage.setItem('local', JSON.stringify(arr));
-    localStorage.setItem('local1', JSON.stringify(arr));
-    localStorage.setItem('local2', JSON.stringify(arr));
-}
-
-remove.addEventListener('click', onRemove)
-
-function onRemove() {
-// localStorage.removeItem('local');
-localStorage.clear();
-}
+console.log('hello world');
